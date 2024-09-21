c34ed195f96610546ecf3ecdb60daa5e34d358d7
Dmytro Wasilewskii <159699205+DiWasilewskii@users.noreply.github.com>
2024-09-21 15:56:53 +0300

    up

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation.jsx";
+import MovieReviews from "./components/MovieReviews/MovieReviews.jsx";
+import MovieCast from "./components/MovieCast/MovieCast.jsx";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const MovePage = lazy(() => import("./pages/MoviesPage/MoviesPage.jsx"));
+const MovieDetailsPage = lazy(
+  () => import("./pages/MovieDetailsPage/MovieDetailsPage.jsx")
+);
+const NotFoundPage = lazy(
+  () => import("./pages/NotFoundPage/NotFoundPage.jsx")
+);
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Suspense fallback={<div>LOADING PAGE...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<MovePage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="reviews" element={<MovieReviews />} />
+            <Route path="cast" element={<MovieCast />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+
+const options = {
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YTdjNDM5OTRmZTY4MmMwOTIyNzE0MzVkMmRjYjBkYyIsIm5iZiI6MTcyNjkyMjU2NS42MTUzNCwic3ViIjoiNjZlZWJlNWM3MzAwYTViYTIxM2IwODAwIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.pDadkGmpsGDucLg1sYZAeqQldOMb_wPzc4-AykOXJNw",
+  },
+};
+
+export async function getMoviesTrending() {
+  const response = await axios.get(
+    "/trending/movie/day?language=en-US",
+    options
+  );
+  return response.data.results;
+}
+
+export async function getMoviesDetails(movieId) {
+  const response = await axios.get(`/movie/${movieId}?language=en-US`, options);
+  return response.data;
+}
+
+export async function getMoviesReviews(movieId, param) {
+  const response = await axios.get(
+    `/movie/${movieId}/${param}?language=en-US`,
+    options
+  );
+  return response.data;
+}
+
+export async function getMoviesSearch(moviesSearch) {
+  const response = await axios.get(
+    `/search/movie?query=${moviesSearch}&include_adult=false&language=en-US&page=1`,
+    options
+  );
+  return response.data;
+}
